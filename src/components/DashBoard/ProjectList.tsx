93ba339285696881e36562aa6d74df42b729e4ce
Ahmed Hairon <a.hairon@bimventures.com>
2022-09-18 15:01:33 +0300

    before changing everything to services

--- src/components/DashBoard/ProjectList.tsx
+++ src/components/DashBoard/ProjectList.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 import ProjectItemContainer from "./ProjectItemContainer";
 import { ProjectsContext } from "../../services/Projects/Projects.context";
+import { fetchUserProjects } from "../../services/Projects/Projects.service";
 
 const Container = styled(Box)`
   border: 1px solid green;
@@ -12,26 +13,19 @@ const Container = styled(Box)`
 
 const ProjectList = () => {
   const Projects = useContext(ProjectsContext);
-  console.log(Projects?.projectList);
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const data = await fetchUserProjects();
+      Projects?.setProjects(data);
+    };
+  }, []);
 
   const ProjectList = Projects?.projectList?.map((project) => {
-    return (
-      <>
-        <ProjectItemContainer project={project} key={project.id} />;
-        <img
-          src={`https://3404-2001-16a2-ce63-9900-14b9-fe82-ff2b-eafa.eu.ngrok.io/storage/projects/image/potato`}
-        ></img>
-        <div>{project.name}</div>
-      </>
-    );
+    return <ProjectItemContainer project={project} key={project.id} />;
   });
 
-  return (
-    <Container>
-      <ProjectItemContainer></ProjectItemContainer>
-      {ProjectList}
-    </Container>
-  );
+  return <Container>{ProjectList}</Container>;
 };
 
 export default ProjectList;
